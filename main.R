77d2659bb1bad4663b0369c590b940fd252be61d
shilohd <daniel.shiloh@ons.gov.uk>
2022-12-29 16:28:30 +0000

    Working quarto skeleton

--- main.R
+++ main.R
@@ -2,4 +2,15 @@ library(magrittr)
 
 data <- CARS::get_tidy_data() %>% CARS::rename_cols() %>% CARS::apply_skip_logic() %>% CARS::derive_vars()
 
-tables <- CARS::summarise_all(data, all_tables = TRUE)
+render_site <- function(data, path = "quarto/main") {
+  tables <- CARS::summarise_all(data, all_tables = TRUE)
+
+  dir.create(paste0(path, "/temp"))
+  save(tables, file = paste0(path, "/temp/summary_tables.Rda"))
+
+  quarto::quarto_render(input = path, as_job = FALSE)
+
+  unlink(paste0(path, "/temp"), recursive = TRUE)
+}
+
+
