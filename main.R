c4504d0975fce1150d564fb552cb673b87e4be14
shilohd <daniel.shiloh@ons.gov.uk>
2023-07-28 18:27:19 +0100

    bugfix: broken link

--- main.R
+++ main.R
@@ -1,3 +1,11 @@
+library(magrittr)
+
+data <- CARS::get_tidy_data_file("2022_data.csv") %>%
+  CARS::rename_cols() %>%
+  CARS::apply_skip_logic() %>%
+  CARS::clean_departments() %>%
+  CARS::derive_vars()
+
 CARS::create_filtered_pages(data, type = "departments")
 CARS::create_filtered_pages(type = "professions")
 CARS::render_site()
