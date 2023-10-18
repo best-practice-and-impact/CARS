dbd6af265ea8bdc8b6ed6ff9db94a3b30afbc1ff
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-10-18 15:17:01 +0100

    Update question routing for new survey

--- R/question-routing.R
+++ R/question-routing.R
@@ -11,21 +11,50 @@
 
 apply_skip_logic <- function(data) {
 
-  conditions <- list(data$workplace %in% c("Civil service, including devolved administations", "test"),
+  conditions <- list(data$workplace != "NHS or local healthcare service",
+                     data$workplace %in%
+                       c("Civil service, including devolved administations",
+                         "NHS or local healthcare service",
+                         "test"),
+                     data$department != "Office for National Statistics",
                      data$department %in% c("Office for National Statistics", "test"),
+                     !is.na(data$ONS_directorate),
+                     data$pay_band != "Local Authority or NJC",
+                     data$pay_band != "Other / Not sure",
+                     !is.na(data$NHS_band),
+                     data$primary_work_country != "Scotland",
+                     data$primary_work_country != "Wales",
+                     data$primary_work_country != "Notherin Ireland",
+                     !is.na(data$England_NHS_organisation),
+                     !is.na(data$Scotland_NHS_organisation),
+                     !is.na(data$Wales_NHS_organisation),
                      data$highest_qualification != "Any other qualification",
                      data$code_freq != "Never",
                      data$other_coding_experience != "No",
-                     data$prev_coding_experience != "No",
-                     data$heard_of_RAP != "No")
+                     data$heard_of_RAP != "No",
+                     data$have_RAP_champ %in% c("Yes", "test"),
+  )
 
   skipped_cols <- list(colnames(data)[which(colnames(data) == "CS_grade"):which(colnames(data) == "ONS_directorate")],
-                       colnames(data)[which(colnames(data) == "ONS_directorate")],
+                       colnames(data)[which(colnames(data) == "CS_grade"):which(colnames(data) == "Northern_Ireland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "prof_DE"):which(colnames(data) == "prof_CS_other")],
+                       colnames(data)[which(colnames(data) == "prof_DE"):which(colnames(data) == "Northern_Ireland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "pay_band"):which(colnames(data) == "Northern_Ireland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "NHS_band")],
+                       colnames(data)[which(colnames(data) == "NHS_band"):which(colnames(data) == "NJC_grade")],
+                       colnames(data)[which(colnames(data) == "NJC_grade")],
+                       colnames(data)[which(colnames(data) == "England_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "England_NHS_organisation"):which(colnames(data) == "Scotland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "England_NHS_organisation"):which(colnames(data) == "Wales_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "Scotland_NHS_organisation"):which(colnames(data) == "Northern_Ireland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "Wales_NHS_organisation"):which(colnames(data) == "Northern_Ireland_NHS_organisation")],
+                       colnames(data)[which(colnames(data) == "Northern_Ireland_NHS_organisation")],
                        colnames(data)[which(colnames(data) == "qual_1_subject"):which(colnames(data) == "qual_3_learn_code")],
-                       colnames(data)[which(colnames(data) == "prac_use_open_source"):which(colnames(data) == "misc_coding")],
-                       colnames(data)[which(colnames(data) == "coding_ability_change"):which(colnames(data) == "first_learned")],
+                       colnames(data)[which(colnames(data) == "line_manage"):which(colnames(data) == "reproducible_workflow")],
                        colnames(data)[which(colnames(data) == "first_learned")],
-                       colnames(data)[which(colnames(data) == "know_RAP_champ"):which(colnames(data) == "RAP_comments")])
+                       colnames(data)[which(colnames(data) == "have_RAP_champ"):which(colnames(data) == "RAP_comments")],
+                       colnames(data)[which(colnames(data) == "know_RAP_champ")],
+  )
 
   for(i in 1:length(conditions)){
     data <- enforce_skip_logic(data, conditions[[i]], skipped_cols[[i]])
