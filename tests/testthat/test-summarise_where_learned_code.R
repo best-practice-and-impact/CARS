d03e535620b25be81e9cf1987c0d4fcc893ddb70
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-11-01 13:38:05 +0000

    Updated tests

--- tests/testthat/test-summarise_where_learned_code.R
+++ tests/testthat/test-summarise_where_learned_code.R
@@ -1,10 +1,12 @@
-dummy_data <- data.frame(code_freq = c(rep("Never", 8), "Sometimes", "Regularly"),
-                         other_coding_experience = c(rep("Yes", 8), "No", "Yes"),
-                         prev_coding_experience = c(rep("Yes", 8), NA, "No"),
-                         first_learned = c(rep("Self-taught" , 3),
-                                           rep( "In public sector employment", 3),
-                                           rep("other" , 1),
-                                           rep(NA , 3)))
+dummy_data <- data.frame(code_freq = rep(c(NA, "Never", "Rarely","Sometimes", "Regularly", "All the time"), each=6),
+                         other_coding_experience = rep(c(NA, "Yes", "No"), 12),
+                         prev_coding_experience = rep(c(NA, "Yes", "No"), 12),
+                         first_learned = c(rep(NA, 6),
+                                           rep("Current employment", 6),
+                                           rep("Education", 6),
+                                           rep("Previous private sector employment", 6),
+                                           rep("Previous public sector employment", 6),
+                                           rep("Other", 6)))
 
 test_that("summarise_where_learned_code missing data is handled correctly", {
 
@@ -18,19 +20,17 @@ test_that("summarise_where_learned_code output is as expected", {
 
   got <- summarise_where_learned_code(dummy_data)
 
-  expected <- data.frame(value = factor(c("In current role",
-                                          "In education",
-                                          "In private sector employment",
-                                          "In public sector employment",
-                                          "Self-taught",
+  expected <- data.frame(value = factor(c("Current employment",
+                                          "Education",
+                                          "Previous private sector employment",
+                                          "Previous public sector employment",
                                           "Other"),
-                                        levels = c("In current role",
-                                                   "In education",
-                                                   "In private sector employment",
-                                                   "In public sector employment",
-                                                   "Self-taught",
+                                        levels = c("Current employment",
+                                                   "Education",
+                                                   "Previous private sector employment",
+                                                   "Previous public sector employment",
                                                    "Other")),
-                         n = c(2/9, 0, 0, 1/3, 1/3, 1/9))
+                         n = rep(1/5, times=5))
 
   expect_equal(got, expected)
 
@@ -62,14 +62,5 @@ test_that("summarise_where_learned_code validation works", {
                                                rep(NA , 3)))
 
   expect_error(summarise_where_learned_code(dummy_data_3), "unexpected_input: no column called 'code_freq'")
-
-  dummy_data_4 <- data.frame(code_freq = c(rep("Never", 8), "Sometimes", "Regularly"),
-                             other_coding_experience = c(rep("Yes", 8), "No", "Yes"),
-                             first_learned = c(rep("Self-taught" , 3),
-                                               rep( "In public sector employment", 3),
-                                               rep("other" , 1),
-                                               rep(NA , 3)))
-
-  expect_error(summarise_where_learned_code(dummy_data_4), "unexpected_input: no column called 'prev_coding_experience'")
 
 })
