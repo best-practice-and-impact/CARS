f46c246961fde8831593de92274f35c720de86bc
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-11-24 09:59:39 +0000

    Update summarise_cap_change_by_CS_grade test

--- tests/testthat/test-summarise_cap_change_by_CS_grade.R
+++ tests/testthat/test-summarise_cap_change_by_CS_grade.R
@@ -1,15 +1,20 @@
-dummy_data <- data.frame(CS_grade = c(NA,
-                                      rep("Higher Executive Officer (or equivalent)", 10),
-                                      rep("Senior Executive Officer (or equivalent)", 10),
-                                      rep("Grade 7 (or equivalent)", 5),
-                                      rep("Grade 6 (or equivalent)", 5)),
-                         coding_ability_change = c(NA,
-                                                   rep(c("Significantly worse",
-                                                       "Slightly worse",
-                                                       "No change",
-                                                       "Slightly better",
-                                                       "Significantly better"),
-                                                     times = 6)))
+dummy_data <- data.frame(
+  CS_grade = c(
+    NA,
+    rep("Higher Executive Officer (or equivalent)", 10),
+    rep("Senior Executive Officer (or equivalent)", 10),
+    rep("Grade 7 (or equivalent)", 5),
+    rep("Grade 6 (or equivalent)", 5)),
+  coding_ability_change = c(
+    NA,
+    rep(c(
+      "It has become significantly worse",
+      "It has become slightly worse",
+      "It has stayed the same",
+      "It has become slightly better",
+      "It has become significantly better"),
+      times = 6))
+  )
 
 test_that("summarise_cap_change_by_CS_grade missing data is handled correctly", {
 
@@ -23,26 +28,34 @@ test_that("summarise_cap_change_by_CS_grade output is as expected", {
 
   got <- summarise_cap_change_by_CS_grade(dummy_data)
 
-  expected <- data.frame(CS_grade = factor(rep(c("Higher Executive Officer (or equivalent)",
-                                                  "Senior Executive Officer (or equivalent)",
-                                                  "Grade 6 and 7"),
-                                           each = 5),
-                                           levels = c("Higher Executive Officer (or equivalent)",
-                                                     "Senior Executive Officer (or equivalent)",
-                                                     "Grade 6 and 7")),
-                         coding_ability_change = factor(rep(c("Significantly worse",
-                                                                "Slightly worse",
-                                                                "No change",
-                                                                "Slightly better",
-                                                                "Significantly better"), 3),
-                                                            levels = c("Significantly worse",
-                                                                       "Slightly worse",
-                                                                       "No change",
-                                                                       "Slightly better",
-                                                                       "Significantly better")),
-                         n = c(0.20, 0.20, 0.20, 0.20, 0.20,
-                               0.20, 0.20, 0.20, 0.20, 0.20,
-                               0.20, 0.20, 0.20, 0.20, 0.20))
+  expected <- data.frame(
+    CS_grade = factor(rep(c(
+      "Higher Executive Officer (or equivalent)",
+      "Senior Executive Officer (or equivalent)",
+      "Grade 6 and 7"),
+      each = 5),
+      levels = c(
+        "Higher Executive Officer (or equivalent)",
+        "Senior Executive Officer (or equivalent)",
+        "Grade 6 and 7")),
+    coding_ability_change = factor(rep(c(
+      "It has become significantly worse",
+      "It has become slightly worse",
+      "It has stayed the same",
+      "It has become slightly better",
+      "It has become significantly better"),
+      times = 3),
+      levels = c(
+        "It has become significantly worse",
+        "It has become slightly worse",
+        "It has stayed the same",
+        "It has become slightly better",
+        "It has become significantly better")),
+      n = c(
+         0.20, 0.20, 0.20, 0.20, 0.20,
+         0.20, 0.20, 0.20, 0.20, 0.20,
+         0.20, 0.20, 0.20, 0.20, 0.20)
+    )
 
 
   expect_equal(got, expected)
