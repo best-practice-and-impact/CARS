6b2fd354e0f7d933c1bba7894d113c2fb033d705
ldavies99 <Luke.Davies@ext.ons.gov.uk>
2023-11-24 11:21:43 +0000

    Update summarise_rep_workflow test

--- tests/testthat/test-summarise_rep_workflow.R
+++ tests/testthat/test-summarise_rep_workflow.R
@@ -1,7 +1,12 @@
-dummy_data <- data.frame(reproducible_workflow = c(NA,
-                                                   rep("Yes", 2),
-                                                   rep("No", 3),
-                                                   rep("I don't know what reproducible workflows are", 4)))
+dummy_data <- data.frame(
+
+  reproducible_workflow = c(
+    NA,
+    "Yes",
+    "No",
+    "I don't know")
+
+)
 
 test_that("summarise_rep_workflow missing data is handled correctly", {
 
@@ -15,13 +20,19 @@ test_that("summarise_rep_workflow output is as expected", {
 
   got <- summarise_rep_workflow(dummy_data)
 
-  expected <- data.frame(value = factor(c("Yes",
-                                          "No",
-                                          "I don't know what reproducible workflows are"),
-                                        levels = c("Yes",
-                                                   "No",
-                                                   "I don't know what reproducible workflows are")),
-                         n = c(2/9, 1/3, 4/9))
+  expected <- data.frame(
+
+    value = factor(c(
+      "Yes",
+      "No",
+      "I don't know"),
+      levels = c("Yes",
+                 "No",
+                 "I don't know")),
+
+    n = rep(1/3, times = 3)
+
+  )
 
   expect_equal(got, expected)
 
@@ -32,6 +43,7 @@ test_that("summarise_rep_workflow validation works", {
 
   dummy_data <- data.frame(Test = c("test1", "test2"))
 
-  expect_error(summarise_rep_workflow(dummy_data), "unexpected_input: no column called 'reproducible_workflow'")
+  expect_error(summarise_rep_workflow(dummy_data),
+               "unexpected_input: no column called 'reproducible_workflow'")
 
 })
