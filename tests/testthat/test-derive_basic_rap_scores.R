456cad1f453f662b76966f28fe03c9cff8367c35
Rowley <Charlotte.Rowley@ons.gov.uk>
2024-01-04 09:09:43 +0000

    updated derive_basic_rap_scores test

--- tests/testthat/test-derive_basic_rap_scores.R
+++ tests/testthat/test-derive_basic_rap_scores.R
@@ -3,7 +3,7 @@ dummy_data <- data.frame(code_freq = c("Never", rep("Sometimes", 4)),
                          prac_open_source_own = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"),
                          prac_version_control = c(NA, "Never" ,"All the time", "Regularly", "Sometimes"),
                          prac_review = c(NA, "Rarely", "Never", "All the time", "Regularly"),
-                         prac_development_QA = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
+                         prac_proportionate_QA = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
                          doc_readme = c(NA, "Regularly", "Sometimes", "Rarely", "Never"),
                          doc_comments = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"))
 
@@ -24,14 +24,14 @@ test_that("derive_basic_rap_scores output is as expected", {
                          prac_open_source_own = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"),
                          prac_version_control = c(NA, "Never" ,"All the time", "Regularly", "Sometimes"),
                          prac_review = c(NA, "Rarely", "Never", "All the time", "Regularly"),
-                         prac_development_QA = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
+                         prac_proportionate_QA = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
                          doc_readme = c(NA, "Regularly", "Sometimes", "Rarely", "Never"),
                          doc_comments = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"),
                          use_open_source_score = c(NA, 1, 0, 0, 0),
                          open_code_score = c(NA, 1, 1, 0, 0),
                          version_control_score = c(NA, 0, 1, 1, 0),
                          peer_review_score = c(NA, 0, 0, 1, 1),
-                         development_QA_score = c(NA, 0, 0, 0, 1),
+                         proportionate_QA_score = c(NA, 0, 0, 0, 1),
                          doc_score = c(NA, 1, 0, 0, 0),
                          basic_rap_score = c(NA, 3, 2, 2, 2))
 
