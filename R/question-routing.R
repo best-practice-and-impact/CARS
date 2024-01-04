32f38555f74206faa2ad426e20a5738951be25eb
daviel9 <Luke.Davies@ons.gov.uk>
2024-01-04 10:56:31 +0000

    Make question routing more efficient

--- R/question-routing.R
+++ R/question-routing.R
@@ -30,27 +30,27 @@ apply_skip_logic <- function(data) {
                      data$heard_of_RAP == "No",
                      data$have_RAP_champ != "Yes" & !is.na(data$have_RAP_champ) & data$have_RAP_champ != "test")
 
-  skipped_cols <- list(dplyr::select(data, "CS_grade":"ONS_directorate"),
-                       dplyr::select(data, "CS_grade":"Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "ONS_directorate":"Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "pay_band":"Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "NHS_band"),
-                       dplyr::select(data, "NHS_band":"NJC_grade"),
-                       dplyr::select(data, "NJC_grade"),
-                       dplyr::select(data, "England_NHS_organisation"),
-                       dplyr::select(data, "England_NHS_organisation":"Scotland_NHS_organisation"),
-                       dplyr::select(data, "England_NHS_organisation":"Wales_NHS_organisation"),
-                       dplyr::select(data, "Scotland_NHS_organisation":"Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "Wales_NHS_organisation":"Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "Northern_Ireland_NHS_organisation"),
-                       dplyr::select(data, "qual_1_subject":"qual_3_learn_code"),
-                       dplyr::select(data, "other_coding_experience":"reproducible_workflow"),
-                       dplyr::select(data, "first_learned":"coding_ability_change"),
-                       dplyr::select(data, "have_RAP_champ":"RAP_comments"),
-                       dplyr::select(data, "know_RAP_champ"))
+  skipped_cols <- list(colnames(dplyr::select(data, "CS_grade":"ONS_directorate")),
+                       colnames(dplyr::select(data, "CS_grade":"Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "ONS_directorate":"Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "pay_band":"Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "NHS_band")),
+                       colnames(dplyr::select(data, "NHS_band":"NJC_grade")),
+                       colnames(dplyr::select(data, "NJC_grade")),
+                       colnames(dplyr::select(data, "England_NHS_organisation")),
+                       colnames(dplyr::select(data, "England_NHS_organisation":"Scotland_NHS_organisation")),
+                       colnames(dplyr::select(data, "England_NHS_organisation":"Wales_NHS_organisation")),
+                       colnames(dplyr::select(data, "Scotland_NHS_organisation":"Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "Wales_NHS_organisation":"Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "Northern_Ireland_NHS_organisation")),
+                       colnames(dplyr::select(data, "qual_1_subject":"qual_3_learn_code")),
+                       colnames(dplyr::select(data, "other_coding_experience":"reproducible_workflow")),
+                       colnames(dplyr::select(data, "first_learned":"coding_ability_change")),
+                       colnames(dplyr::select(data, "have_RAP_champ":"RAP_comments")),
+                       colnames(dplyr::select(data, "know_RAP_champ")))
 
   for(i in 1:length(conditions)){
-    data <- enforce_skip_logic(data, conditions[[i]], colnames(skipped_cols[[i]]))
+    data <- enforce_skip_logic(data, conditions[[i]], skipped_cols[[i]])
   }
 
   return(data)
