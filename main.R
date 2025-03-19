d00ad7d4dd06fcfe3806456d0734bb3b69f49388
Rowley <Charlotte.Rowley@ons.gov.uk>
2025-03-19 11:01:08 +0000

    update main file

--- main.R
+++ main.R
@@ -5,8 +5,7 @@ data <- CARS::get_tidy_data_file("2024_data.csv")
 config <- yaml::read_yaml("config.yml")
 
 data <- clean_data(data, config)
-#  CARS::derive_vars()
 
-#CARS::create_filtered_pages(data, type = "departments")
-#CARS::create_filtered_pages(data, type = "professions")
+CARS::create_filtered_pages(data, type = "departments")
+CARS::create_filtered_pages(data, type = "professions")
 #CARS::render_site()
