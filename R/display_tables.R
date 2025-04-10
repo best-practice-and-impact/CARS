bee3d99cd209ef82f3c09dfe32ac134596698270
Rowley <Charlotte.Rowley@ons.gov.uk>
2025-04-10 14:56:00 +0100

    update df_to_table to use config

--- R/display_tables.R
+++ R/display_tables.R
@@ -2,40 +2,43 @@
 #'
 #' @description converts dataframe to presentation HTML table
 #'
-#' @param data frequency table in tidy format. Last column should contain proportions.
-#' First one or two columns should contain factors.
+#' @param data list of CARS frequency tables, table names should match config
+#' @param config CARS config
+#' @param question string, question of interest. Name should match config.
 #' @param column_headers optional: list of column headers for the output table
-#' @param n optional: sample size
 #' @param crosstab whether to create a cross tabulation. FALSE by default
-#' @param proportion_col optional: columns to be turned into a percentage. Last column only be default
 #' @return HTML table
 #' @export
 
-df_to_table <- function(data, column_headers, n, crosstab = FALSE, proportion_col) {
+df_to_table <- function(data, config, question, crosstab = FALSE, column_headers) {
 
-  if (missing(proportion_col)) {
-    proportion_col <- length(data)
-  }
+  list2env(get_question_data(config, question), envir = environment())
 
-  data[proportion_col] <- round(data[proportion_col] * 100, 1) %>% lapply(paste0, "%")
+  data <- data[[cols]]
+
+  table_data <- dplyr::select(data, !dplyr::any_of(c("count", "sample")))
+
+  table_data["n"] <- round(table_data["n"] * 100, 1) |> lapply(paste0, "%")
 
   if (crosstab) {
-    data <- df_to_crosstab(data)
+    table_data <- df_to_crosstab(table_data)
 
-    alignment <- c("l", rep("r", ncol(data)-1))
+    alignment <- c("l", rep("r", ncol(table_data)-1))
   } else {
-    alignment <- c(rep("l", ncol(data)-1), "r")
+    alignment <- c(rep("l", ncol(table_data)-1), "r")
   }
 
   if (!missing(column_headers)) {
-    colnames(data) <- column_headers
+    colnames(table_data) <- column_headers
+  } else {
+    colnames(table_data) <- c(full_question, "Percentage")
   }
 
-  html <- knitr::kable(data, align = alignment, format = "html") %>% kableExtra::kable_styling()
+  html <- knitr::kable(table_data, align = alignment, format = "html") %>% kableExtra::kable_styling()
 
-  if (!missing(n)) {
-    html <- kableExtra::add_footnote(html, paste0("Sample size = ", n), notation = "none")
-  }
+
+  html <- kableExtra::add_footnote(html, paste0("Sample size = ", data$sample[1]), notation = "none")
+
 
   return(html)
 }
