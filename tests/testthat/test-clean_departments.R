34215450b9d2e6ec8ccbeb48e10c7ff120914743
Rowley <Charlotte.Rowley@ons.gov.uk>
2025-05-08 14:34:11 +0100

    update clean_department test

--- tests/testthat/test-clean_departments.R
+++ tests/testthat/test-clean_departments.R
@@ -3,7 +3,6 @@ test_that("clean_departments output is as expected", {
 
   dummy_data <- data.frame(department = c(NA,
                                           "test",
-                                          "Foreign, Commonwealth & Development Office (excl. agencies)",
                                           "Department for Environment, Food and Rural Affairs (excl. agencies)",
                                           "Forestry Commission",
                                           "Forest Research",
@@ -14,13 +13,12 @@ test_that("clean_departments output is as expected", {
                                           "Environment Agency",
                                           "Marine Management Organisation",
                                           "Natural England"),
-                           workplace = c(NA, "NHS", rep("test", 11)))
+                           workplace = c(NA, "NHS or local healthcare service", rep("test", 10)))
 
   got <- clean_departments(dummy_data)
 
   expected <- data.frame(department = c(NA,
                                         "NHS",
-                                        "Foreign, Commonwealth and Development Office (excl. agencies)",
                                         "Department for Environment, Food and Rural Affairs (excl. agencies)",
                                         "Forestry Commission",
                                         "Forest Research",
@@ -31,8 +29,8 @@ test_that("clean_departments output is as expected", {
                                         "Environment Agency",
                                         "Marine Management Organisation",
                                         "Natural England"),
-                         workplace = c(NA, "NHS", rep("test", 11)),
-                         defra = c(rep(FALSE, 3), rep(TRUE, 10)))
+                         workplace = c(NA, "NHS or local healthcare service", rep("test", 10)),
+                         defra = c(rep(FALSE, 2), rep(TRUE, 10)))
 
   expect_equal(got, expected)
 
