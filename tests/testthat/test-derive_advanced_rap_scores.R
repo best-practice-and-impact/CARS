5568b0d7b99148613e4455e97c6280abbefa4180
Rowley <Charlotte.Rowley@ons.gov.uk>
2025-05-08 15:27:22 +0100

    update adv_rap_score test

--- tests/testthat/test-derive_advanced_rap_scores.R
+++ tests/testthat/test-derive_advanced_rap_scores.R
@@ -1,18 +1,19 @@
 
 dummy_data <- data.frame(code_freq = c("Never", rep("Sometimes", 4)),
                          prac_functions = c(NA, "Regularly", "Sometimes", "Rarely", "Never"),
-                         prac_unit_test = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"),
-                         doc_functions = c(NA, "Never", "All the time", "Regularly", "Sometimes"),
-                         prac_package = c(NA, "Rarely", "Never", "All the time", "Regularly"),
-                         prac_style = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
-                         CI = c(NA, "No", "Yes", "No", "I don't know what continuous integration is"),
-                         dep_management = c(NA, "I don't know what dependency management is", "No", "Yes", "No"))
+                         prac_manual_tests = c(NA, "Always", "Regularly", "Sometimes", "Rarely"),
+                         prac_auto_tests = c(NA, "Always", "Regularly", "Sometimes", "Rarely"),
+                         doc_function = c(NA, "Never", "Always", "Regularly", "Sometimes"),
+                         prac_control_flow = c(NA, "Rarely", "Never", "Always", "Regularly"),
+                         prac_config = c(NA, "Sometimes", "Rarely", "Never", "Always"),
+                         prac_code_style = c(NA, "Sometimes", "Rarely", "Never", "Always"),
+                         doc_dependencies = c(NA, "Rarely", "Always", "Never", "Regularly"))
 
 test_that("derive_advanced_rap_scores validation works", {
 
   dummy_data <- data.frame()
 
-  expect_error(derive_advanced_rap_scores(dummy_data), "Unexpected input - missing column names: code_freq\nprac_functions\nprac_unit_test\ndoc_functions\nprac_package\nprac_style\nCI\ndep_management")
+  expect_error(derive_advanced_rap_scores(dummy_data), "Unexpected input - missing column names: code_freq\nprac_functions\nprac_manual_tests\nprac_auto_tests\ndoc_function\nprac_control_flow\nprac_config\nprac_code_style\ndoc_dependencies")
 
 })
 
@@ -22,20 +23,22 @@ test_that("derive_advanced_rap_scores output is as expected", {
 
   expected <- data.frame(code_freq = c("Never", rep("Sometimes", 4)),
                          prac_functions = c(NA, "Regularly", "Sometimes", "Rarely", "Never"),
-                         prac_unit_test = c(NA, "All the time", "Regularly", "Sometimes", "Rarely"),
-                         doc_functions = c(NA, "Never", "All the time", "Regularly", "Sometimes"),
-                         prac_package = c(NA, "Rarely", "Never", "All the time", "Regularly"),
-                         prac_style = c(NA, "Sometimes", "Rarely", "Never", "All the time"),
-                         CI = c(NA, "No", "Yes", "No", "I don't know what continuous integration is"),
-                         dep_management = c(NA, "I don't know what dependency management is", "No", "Yes", "No"),
+                         prac_manual_tests = c(NA, "Always", "Regularly", "Sometimes", "Rarely"),
+                         prac_auto_tests = c(NA, "Always", "Regularly", "Sometimes", "Rarely"),
+                         doc_function = c(NA, "Never", "Always", "Regularly", "Sometimes"),
+                         prac_control_flow = c(NA, "Rarely", "Never", "Always", "Regularly"),
+                         prac_config = c(NA, "Sometimes", "Rarely", "Never", "Always"),
+                         prac_code_style = c(NA, "Sometimes", "Rarely", "Never", "Always"),
+                         doc_dependencies = c(NA, "Rarely", "Always", "Never", "Regularly"),
                          function_score = c(NA, 1, 0, 0, 0),
-                         unit_test_score = c(NA, 1, 1, 0, 0),
+                         manual_test_score = c(NA, 1, 1, 0, 0),
+                         auto_test_score = c(NA, 1, 1, 0, 0),
                          function_doc_score = c(NA, 0, 1, 1, 0),
-                         package_score = c(NA, 0, 0, 1, 1),
+                         control_flow_score = c(NA, 0, 0, 1, 1),
+                         config_score = c(NA, 0, 0, 0, 1),
                          code_style_score = c(NA, 0, 0, 0, 1),
-                         cont_integration_score = c(NA, 0, 1, 0, 0),
-                         dep_management_score = c(NA, 0, 0, 1, 0),
-                         advanced_rap_score = c(NA, 2, 3, 3, 2))
+                         dep_management_score = c(NA, 0, 1, 0, 1),
+                         advanced_rap_score = c(NA, 3, 4, 2, 4))
 
   expect_equal(got, expected)
 
