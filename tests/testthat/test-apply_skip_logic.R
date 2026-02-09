981c987e31b2da7a30251bee3e2bb9b62fe9055e
rowlec1 <charlotte.rowley@ons.gov.uk>
2026-02-09 15:37:10 +0000

    update file names in apply_skip_logic_test

--- tests/testthat/test-apply_skip_logic.R
+++ tests/testthat/test-apply_skip_logic.R
@@ -1,5 +1,5 @@
 test_that("apply_skip_logic replaces all relevant columns with NAs", {
-  dummy_data <- rename_cols(cars_dummy_data, config)
+  dummy_data <- rename_cols(cars_dummy_data_test, config)
 
   got <- apply_skip_logic(dummy_data)
 
