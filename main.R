8cd3244080430dd23d1cc2add2385c6332f8724c
daviel9 <Luke.Davies@ons.gov.uk>
2026-06-18 14:11:01 +0100

    Remove question routing line

--- main.R
+++ main.R
@@ -5,6 +5,9 @@ config <- yaml::read_yaml("config.yml")
 
 data<- CARS::clean_data(data, config)
 data <- CARS::derive_vars(data)
+data$year <- 2026
+file_path <- file.path(Sys.getenv("CARS_DATA_DIR"), "2026_cleaned_data.rds")
+saveRDS(data, file_path)
 
 CARS::create_filtered_pages(data, type = "departments")
 CARS::create_filtered_pages(data, type = "professions")
