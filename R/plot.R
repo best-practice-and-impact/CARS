62a7eb864403569a3ef8cbe6dd3ddd4636afd0cb
daviel9 <Luke.Davies@ons.gov.uk>
2026-07-06 11:19:09 +0100

    Move piechart percentages to outside of slices

--- R/plot.R
+++ R/plot.R
@@ -245,6 +245,7 @@ plot_freqs <- function(data, config, question, colour, break_q_names_col, type =
       values = abs(data[[2]]),
       type = "pie",
       textinfo = "label+percent",
+      textposition = "outside",
       texttemplate = "<b>%{percent}</b>",
       hovertemplate = "%{label}: %{percent}<extra></extra>",
       marker = list(colors = colours,
